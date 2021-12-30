b09e27170c6dd9b937c96965ffe07414403f9ec3
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2021-12-30 00:41:49 -0500

    solution 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+	let celsius = (temperature - 32) * 5/9;
 
+	// round to 1st decimal
+	return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
+	let fahrenheit = (temperature * 9/5) + 32;
 
+	// round to 1st decimal
+	return Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
