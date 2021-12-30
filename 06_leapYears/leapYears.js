7f9ad6b8d3511750e72680dec6b8f98f430ab456
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2021-12-30 00:33:35 -0500

    solution 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeap = false;
+    if (!(year % 4) && (!(year % 400) || year % 100)) {
+        isLeap = true;
+    }
 
+    return isLeap;
 };
 
 // Do not edit below this line
