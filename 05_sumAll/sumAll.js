e7155a8d6851958ce8a3f925576bacc0e23166dd
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2021-12-30 00:25:52 -0500

    solution 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0 || typeof(first) != "number" || typeof(second) != "number") {
+        return "ERROR";
+    }
+    let low = (first < second) ? first : second;
+    let high = (first >= second) ? first : second;
 
+    let total = 0;
+    for (let i = low; i <= high; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
